a301254ebd8a5a83840b760b4fb1a36ce3f408d5
shushinasabm <98877361+shushinasabm@users.noreply.github.com>
2022-04-26 21:47:01 +0430

    Add search box in header and optimize blog elements

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let up =document.querySelector('.fa-arrow-up')
-
-up.addEventListener('click',function (event) {
+let up = document.querySelector('.fa-arrow-up')
+up.addEventListener('click', function (event) {
     window.scrollTo({
-        top:0,
-behavior:"smooth"
+        top: 0,
+        behavior: "smooth"
     });
-    console.log('click')
+})
 
-    })
+let searchHeader = document.querySelector('.fa-magnifying-glass')
+searchHeader.addEventListener('click', function (event) {
+    let searchBox = document.querySelector('.search-box')
+    searchBox.classList.toggle('search-box-opened')
+})
